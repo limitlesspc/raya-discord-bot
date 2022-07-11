0388e7b0c7ea02bace53ca38cf96c50c2f6c90d3
Izaak Brown <izaakbthunderbird@gmail.com>
2022-07-10 22:17:38 -0400

    chicken.mkv -> chicken.mp4

--- src/commands/chicken.ts
+++ src/commands/chicken.ts
@@ -5,5 +5,5 @@ export default command(
     desc: 'Chicken!',
     options: {}
   },
-  i => i.reply(`${process.env.FILES_ORIGIN}/memes/chicken.mkv`)
+  i => i.reply(`${process.env.FILES_ORIGIN}/memes/chicken.mp4`)
 );
